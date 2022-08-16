890793602510ef061516f2debd189d290454adf0
Gal <galgross24@gmail.com>
2022-08-16 15:29:53 +0300

    רק

--- ReactProjs/foodOrderApp/src/components/Layout/HeaderCartButton.js
+++ ReactProjs/foodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/Cart-context';
 
 const HeaderCartButton = props => {
+    const cartctx = useContext(CartContext);
+
     return(
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
