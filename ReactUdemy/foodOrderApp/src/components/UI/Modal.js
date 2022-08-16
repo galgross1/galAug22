bdeee4d96a0e82302d431096c9e372f804da0bee
Gal <galgross24@gmail.com>
2022-08-16 14:16:23 +0300

    sd

--- ReactUdemy/foodOrderApp/src/components/UI/Modal.js
+++ ReactUdemy/foodOrderApp/src/components/UI/Modal.js
@@ -1,15 +1,18 @@
-import { Fragment } from "react";
-import ReactDOM from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} />;
 };
 
 const ModalOverlay = (props) => {
+    return(
   <div className={classes.modal}>
     <div className={classes.content}>{props.children}</div>
-  </div>;
+  </div>
+  );
 };
 
 const portalElment = document.getElementById("overlays");
