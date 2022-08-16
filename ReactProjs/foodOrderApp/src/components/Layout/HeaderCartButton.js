249076b30849396f4d47cb200578fcda12c44083
Gal <galgross24@gmail.com>
2022-08-16 15:41:04 +0300

    הנמ

--- ReactProjs/foodOrderApp/src/components/Layout/HeaderCartButton.js
+++ ReactProjs/foodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,24 @@
-import { useContext } from 'react';
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
-import CartContext from '../../store/Cart-context';
+import { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/Cart-context";
 
-const HeaderCartButton = props => {
-    const cartctx = useContext(CartContext);
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
 
-    return(
-        <button className={classes.button} onClick={props.onClick}>
-            <span className={classes.icon}>
-            <CartIcon/>
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>
-                3
-            </span>
-        </button>
-    );
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
