012fc8e2f86e2e96dd6cf5ac822941e3432949e7
Gal <galgross24@gmail.com>
2022-08-17 15:45:14 +0300

    sd

--- ReactProjs/foodOrderApp/src/components/Layout/HeaderCartButton.js
+++ ReactProjs/foodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/Cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  
+  const {items} = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump: ''}`;
+
+  useEffect(() => {
+    if(items.length === 0){  
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+   const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300)
+
+    return() => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}>
+    <button className={btnClasses} onClick={props.onClick}>
+      <span clssName={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
@@ -22,3 +44,4 @@ const HeaderCartButton = (props) => {
 };
 
 export default HeaderCartButton;
+
--- ReactProjs/foodOrderApp/src/components/UI/Input.js
+++ ReactProjs/foodOrderApp/src/components/UI/Input.js
@@ -1,12 +1,13 @@
 import classes from "./Input.module.css";
+import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input id={props.input.id} {...props.input}/> {/*we can drop 'props.input.id' cause the ...props.input is do the job but i kept that*/}
+      <input ref={ref} id={props.input.id} {...props.input}/> {/*we can drop 'props.input.id' cause the ...props.input is do the job but i kept that*/}
     </div>
   );
-};
+});
 
 export default Input;
