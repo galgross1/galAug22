7f6cd6d340904be1aa2928c80ecf9303443e6b2d
Gal <galgross24@gmail.com>
2022-08-17 16:14:18 +0300

    tr

--- ReactProjs/behindReact/src/App.js
+++ ReactProjs/behindReact/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState();
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING')
 
   const toggleParagraphHandler = () =>{
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
@@ -12,7 +15,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
