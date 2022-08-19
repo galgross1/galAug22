2dceee6e11066efccfc7805ce3b7772e993eb850
Gal <galgross24@gmail.com>
2022-08-19 17:08:00 +0300

    s

--- ReactProjs/behindReact/src/App.js
+++ ReactProjs/behindReact/src/App.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
-
+  const [allowToggle, setAllowToggle] = useState(false); 
   console.log('APP RUNNING')
 
-  const toggleParagraphHandler = () =>{
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const toggleParagraphHandler = useCallback( () =>{
+    if(allowToggle){
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () =>{
+    setAllowToggle(true);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <Button onClick={allowToggleHandler}>allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
--- ReactProjs/behindReact/src/components/Demo/DemoOutput.js
+++ ReactProjs/behindReact/src/components/Demo/DemoOutput.js
@@ -1,9 +1,10 @@
 import React from "react";
+import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
     return (
-        <p>{props.show ? 'This is new!' : ''}</p>
+        <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>
     );
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
