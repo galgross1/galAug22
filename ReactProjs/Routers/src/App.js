ec34f4f4640e8aee07c46b7210a02918e365a91f
Gal <galgross24@gmail.com>
2022-08-20 16:59:11 +0300

    גכ

--- ReactProjs/Routers/src/App.js
+++ ReactProjs/Routers/src/App.js
@@ -1,12 +1,24 @@
 import {Route} from 'react-router-dom'
-import Welcome from './Components/Welcome';
+import Welcome from './Components/Pages/Welcome';
+import Products from './Components/Pages/Products';
+import MainHeader from './Components/MainHeader';
+import ProductDetail from './Components/Pages/ProductDetail';
 
 function App() {
   return (
     <div>
+      <MainHeader />
+      <main>
       <Route path='/welcome'>
         <Welcome/>
       </Route>
+      <Route path='/products'>
+        <Products/>
+      </Route>
+      <Route path='/product-detail/:productId'>
+        <ProductDetail/>
+      </Route>
+      </main>
     </div>
   );
 }
---
+++ ReactProjs/Routers/src/Components/Pages/Products.js
@@ -0,0 +1,14 @@
+const Products = () => {
+  return (
+    <section>
+      <h1>The Products Page</h1>
+        <ul>
+            <li>Book</li>
+            <li>A Carpet</li>
+            <li>A Online Course</li>
+        </ul>
+    </section>
+  );
+};
+
+export default Products;
